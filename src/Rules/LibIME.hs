70b8b25f79d188adf49fed9f526e9351d722aadc
Rocka <i@rocka.me>
2023-11-04 02:49:45 +0800

    Add libime-jyutping dict and lm rules
    build jyutping with fresh libime

---
+++ src/Rules/LibIME.hs
@@ -0,0 +1,144 @@
+module Rules.LibIME
+  (
+    libIMERule,
+    chineseAddonsRule,
+  )
+where
+
+import Base
+import Data.Maybe (fromJust)
+
+fcitxDataUrl :: String
+fcitxDataUrl = "https://download.fcitx-im.org/data/"
+
+dictNames :: [String]
+dictNames = ["sc", "extb"]
+
+tableDictNames :: [String]
+tableDictNames = ["db", "erbi", "qxm", "wanfeng", "wbpy", "wbx", "zrm", "cj"]
+
+libIMERule :: Rules ()
+libIMERule = do
+  libIMEToolsRule
+  lmRule
+  pinyinDictRule
+  tableDictRule
+  "libime" ~> do
+    copyFile' (outputDir </> "sc.dict") $ outputDir </> "libime" </> "data" </> "sc.dict"
+    copyFile' (outputDir </> "extb.dict") $ outputDir </> "libime" </> "data" </> "extb.dict"
+    copyFile' (outputDir </> "sc.lm") $ outputDir </> "libime" </> "data" </> "zh_CN.lm"
+    copyFile' (outputDir </> "sc.lm.predict") $ outputDir </> "libime" </> "data" </> "zh_CN.lm.predict"
+    forM_ tableDictNames $ \table ->
+      let name = table <.> "main.dict"
+       in copyFile' (outputDir </> name) (outputDir </> "libime" </> "table" </> name)
+
+libIMEToolsRule :: Rules ()
+libIMEToolsRule = do
+  "libime-tools" ~> do
+    let libIMESrc = "libime"
+    cmd_ "cmake" "-B" (libIMESrc </> "build") "-G" "Ninja" 
+      [
+        "-DCMAKE_BUILD_TYPE=Release",
+        "-DCMAKE_INSTALL_PREFIX=" <> outputDir,
+        "-DENABLE_TEST=OFF"
+      ]
+      libIMESrc
+    cmd_ "cmake" "--build" (libIMESrc </> "build") "--target"
+      [
+        "libime_slm_build_binary",
+        "libime_prediction",
+        "libime_pinyindict",
+        "libime_history",
+        "libime_tabledict"
+      ]
+    -- ignore install errors
+    Exit c <- cmd "cmake" "--install" (libIMESrc </> "build") "--component" "lib"
+    Exit c <- cmd "cmake" "--install" (libIMESrc </> "build") "--component" "header"
+    Exit c <- cmd "cmake" "--install" (libIMESrc </> "build") "--component" "tools"
+    Exit c <- cmd "cmake" "--install" (libIMESrc </> "build") "--component" "Devel"
+    cmd_ "echo" ("installed to " <> outputDir)
+
+lmRule :: Rules ()
+lmRule = do
+  outputDir </> "lm_sc.arpa" %> \out -> do
+    src <- getConfig' "lm_sc"
+    sha256 <- getConfig' "lm_sc_sha256"
+    tar <- download fcitxDataUrl src sha256
+    cmd_ "tar" "xf" tar "-C" outputDir (takeFileName out)
+  outputDir </> "sc.lm" %> \out -> do
+    let src = outputDir </> "lm_sc.arpa"
+    need ["libime-tools", src]
+    cmd_ (AddEnv "LD_LIBRARY_PATH" (outputDir </> "lib")) (outputDir </> "bin" </> "libime_slm_build_binary") "-s -a 22 -q 8 trie" src out
+  outputDir </> "sc.lm.predict" %> \out -> do
+    let src1 = outputDir </> "sc.lm"
+        src2 = outputDir </> "lm_sc.arpa"
+    need ["libime-tools", src1, src2]
+    cmd_ (AddEnv "LD_LIBRARY_PATH" (outputDir </> "lib")) (outputDir </> "bin" </> "libime_prediction") src1 src2 out
+
+pinyinDictRule :: Rules ()
+pinyinDictRule = do
+  (outputDir </>) . ("dict_" <>) . (<.> "txt") <$> dictNames |%> \out -> do
+    src <- getConfig' "pinyin_dict"
+    sha256 <- getConfig' "pinyin_dict_sha256"
+    tar <- download fcitxDataUrl src sha256
+    (Stdout txt) <- cmd "tar" "xf" tar "-C" outputDir (takeFileName out)
+    produces $ lines txt
+  (outputDir </>) . (<.> "dict") <$> dictNames |%> \out -> do
+    let src = outputDir </> "dict_" <> takeBaseName out <.> "txt"
+    need ["libime-tools", src]
+    cmd_ (AddEnv "LD_LIBRARY_PATH" (outputDir </> "lib")) (outputDir </> "bin" </> "libime_pinyindict") src out
+
+tableDictRule :: Rules ()
+tableDictRule = do
+  (outputDir </>) . (<.> "txt") <$> tableDictNames |%> \out -> do
+    src <- getConfig' "table"
+    sha256 <- getConfig' "table_sha256"
+    tar <- download fcitxDataUrl src sha256
+    (Stdout txt) <- cmd "tar" "xf" tar "-C" outputDir (takeFileName out)
+    produces $ lines txt
+  (outputDir </>) . (<.> "main.dict") <$> tableDictNames |%> \out -> do
+    let src = outputDir </> fromJust (stripExtension "main.dict" $ takeFileName out) <.> "txt"
+    need ["libime-tools", src]
+    cmd_ (AddEnv "LD_LIBRARY_PATH" (outputDir </> "lib")) (outputDir </> "bin" </> "libime_tabledict") src out
+
+--------------------------------------------------------------------------------
+
+chineseAddonsRule :: Rules ()
+chineseAddonsRule = do
+  pinyinExtraDictRule
+  pinyinStrokeRule
+  pinyinTableRule
+  "chinese-addons-data" ~> do
+    copyFile' (outputDir </> "emoji.dict") $ outputDir </> "chinese-addons-data" </> "pinyin" </> "emoji.dict"
+    copyFile' (outputDir </> "chaizi.dict") $ outputDir </> "chinese-addons-data" </> "pinyin" </> "chaizi.dict"
+    copyFile' (outputDir </> "py_table.mb") $ outputDir </> "chinese-addons-data" </> "pinyinhelper" </> "py_table.mb"
+    copyFile' (outputDir </> "py_stroke.mb") $ outputDir </> "chinese-addons-data" </> "pinyinhelper" </> "py_stroke.mb"
+
+pinyinExtraDictRule :: Rules ()
+pinyinExtraDictRule = do
+  let dict name =
+        outputDir </> name <.> "dict" %> \out -> do
+          need ["libime-tools"]
+          let src = name <.> "txt"
+          chineseAddonsRepoDataUrl <- getConfig' "chinese_addon_repo"
+          sha256 <- getConfig' $ name <> "_sha256"
+          txt <- download chineseAddonsRepoDataUrl src sha256
+          cmd_ (AddEnv "LD_LIBRARY_PATH" (outputDir </> "lib")) (outputDir </> "bin" </> "libime_pinyindict") txt out
+  dict "chaizi"
+  dict "emoji"
+
+pinyinStrokeRule :: Rules ()
+pinyinStrokeRule = do
+  outputDir </> "py_stroke.mb" %> \out -> do
+    src <- getConfig' "py_stroke"
+    sha256 <- getConfig' "py_stroke_sha256"
+    tar <- download fcitxDataUrl src sha256
+    cmd_ "tar" "xf" tar "-C" outputDir (takeFileName out)
+
+pinyinTableRule :: Rules ()
+pinyinTableRule = do
+  outputDir </> "py_table.mb" %> \out -> do
+    src <- getConfig' "py_table"
+    sha256 <- getConfig' "py_table_sha256"
+    tar <- download fcitxDataUrl src sha256
+    cmd_ "tar" "xf" tar "-C" outputDir (takeFileName out)
---
+++ src/Rules/LibIMEJyutping.hs
@@ -0,0 +1,66 @@
+module Rules.LibIMEJyutping (libIMEJyutpingRule) where
+
+import Base
+
+fcitxDataUrl :: String
+fcitxDataUrl = "https://download.fcitx-im.org/data/"
+
+libIMEJyutpingRule :: Rules ()
+libIMEJyutpingRule = do
+  jyutpingToolsRule
+  jyutpingDictRule
+  jyutpingLmRule
+  "libime-jyutping" ~> do
+    copyFile' (outputDir </> "jyutping.dict") $ outputDir </> "libime-jyutping" </> "jyutping.dict"
+    copyFile' (outputDir </> "zh_HK.lm") $ outputDir </> "libime-jyutping" </> "zh_HK.lm"
+    copyFile' (outputDir </> "zh_HK.lm.predict") $ outputDir </> "libime-jyutping" </> "zh_HK.lm.predict"
+
+jyutpingToolsRule :: Rules ()
+jyutpingToolsRule = do
+  "libime-jyutping-tools" ~> do
+    need ["libime-tools"]
+    let libIMEJyutpingSrc = "libime-jyutping"
+    cmd_ "sed" "-i" "46{\\|find_package(Fcitx5Module|d}" (libIMEJyutpingSrc </> "CMakeLists.txt")
+    cmd_ "cmake" "-B" (libIMEJyutpingSrc </> "build") "-G" "Ninja"
+      [
+        "-DCMAKE_BUILD_TYPE=Release",
+        "-DCMAKE_INSTALL_PREFIX=" <> outputDir,
+        "-DLibIMECore_DIR=" <> outputDir </> "lib" </> "cmake" </> "LibIMECore",
+        "-DENABLE_TEST=OFF",
+        "-DENABLE_ENGINE=OFF"
+      ]
+      libIMEJyutpingSrc
+    cmd_ "cmake" "--build" (libIMEJyutpingSrc </> "build") "--target" "libime_jyutpingdict"
+    -- ignore install errors
+    Exit c <- cmd "cmake" "--install" (libIMEJyutpingSrc </> "build") "--component" "lib"
+    Exit c <- cmd "cmake" "--install" (libIMEJyutpingSrc </> "build") "--component" "tools"
+    cmd_ "echo" ("installed to " <> outputDir)
+
+jyutpingDictRule :: Rules ()
+jyutpingDictRule = do
+  outputDir </> "words.txt" %> \out -> do
+    src <- getConfig' "jyutping_dict"
+    sha256 <- getConfig' "jyutping_dict_sha256"
+    tar <- download fcitxDataUrl src sha256
+    cmd_ "tar" "xf" tar "-C" outputDir (takeFileName out)
+  outputDir </> "jyutping.dict" %> \out -> do
+    let src = outputDir </> "words.txt"
+    need ["libime-jyutping-tools", src]
+    cmd_ (AddEnv "LD_LIBRARY_PATH" (outputDir </> "lib")) (outputDir </> "bin" </> "libime_jyutpingdict") src out
+
+jyutpingLmRule :: Rules ()
+jyutpingLmRule = do
+  outputDir </> "hk.arpa" %> \out -> do
+    src <- getConfig' "jyutping_model"
+    sha256 <- getConfig' "jyutping_model_sha256"
+    tar <- download fcitxDataUrl src sha256
+    cmd_ "tar" "xf" tar "-C" outputDir (takeFileName out)
+  outputDir </> "zh_HK.lm" %> \out -> do
+    let src = outputDir </> "hk.arpa"
+    need ["libime-tools", src]
+    cmd_ (AddEnv "LD_LIBRARY_PATH" (outputDir </> "lib")) (outputDir </> "bin" </> "libime_slm_build_binary") "-s -a 22 -q 8 trie" src out
+  outputDir </> "zh_HK.lm.predict" %> \out -> do
+    let src1 = outputDir </> "zh_HK.lm"
+        src2 = outputDir </> "hk.arpa"
+    need ["libime-tools", src1, src2]
+    cmd_ (AddEnv "LD_LIBRARY_PATH" (outputDir </> "lib")) (outputDir </> "bin" </> "libime_prediction") src1 src2 out
