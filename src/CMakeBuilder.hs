d6049c8b549b71349d55427db257d05aba795d66
Rocka <i@rocka.me>
2023-12-31 17:16:57 +0800

    Don't use Android NDK legacy CMake toolchain

--- src/CMakeBuilder.hs
+++ src/CMakeBuilder.hs
@@ -114,6 +114,7 @@ useCMake CmakeBuilder {..} = addOracle $ \(WithAndroidEnv q env) -> do
         buildEnvBuildDir
         "-GNinja"
         ( [ "-DCMAKE_TOOLCHAIN_FILE=" <> toolchain,
+            "-DANDROID_USE_LEGACY_TOOLCHAIN_FILE=OFF",
             "-DCMAKE_MAKE_PROGRAM=" <> ninja,
             "-DANDROID_ABI=" <> a,
             "-DANDROID_PLATFORM=" <> show (platform env),
