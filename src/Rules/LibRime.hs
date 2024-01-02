43a4274af60716f5e66b428d55d9efb9b95b2cf5
Rocka <i@rocka.me>
2024-01-02 22:34:48 +0800

    Patch librime and its plugins using actual patch

--- src/Rules/LibRime.hs
+++ src/Rules/LibRime.hs
@@ -30,16 +30,15 @@ librimeRule = do
               createDirectoryLink librimeLuaSrc (src </> "plugins" </> "lua")
               removePathForcibly (src </> "plugins" </> "octagram")
               createDirectoryLink librimeOctagramSrc (src </> "plugins" </> "octagram")
-            -- use prebuilt lua
-            cmd_ (Cwd (src </> "plugins" </> "lua")) Shell "sed -i '11s|^\\s*if(LUA_FOUND)|set(LUA_FOUND 1)\\nset(LUA_INCLUDE_DIRS \"${CMAKE_CURRENT_SOURCE_DIR}/../build/lua/${ANDROID_ABI}/include\")\\n\\0|' CMakeLists.txt"
+            -- find lua with find_package; remove absolute path by __FILE__ macro
+            cmd_ (Cwd librimeLuaSrc) "git checkout ."
+            cmd_ (Cwd librimeLuaSrc) "git apply ../patches/librime-lua.patch"
+            -- disable tools; remove absolute path by __FILE__ macro
+            cmd_ (Cwd librimeOctagramSrc) "git checkout ."
+            cmd_ (Cwd librimeOctagramSrc) "git apply ../patches/librime-octagram.patch"
             -- remove absolute path by __FILE__ macro
-            cmd_ (Cwd (src </> "plugins" </> "lua")) Shell "sed -i '47s|^set(plugin_name.*|target_compile_options(rime-lua-objs PRIVATE \"-ffile-prefix-map=${CMAKE_SOURCE_DIR}=.\")\\n\\0|' CMakeLists.txt"
-            -- remove absolute path by __FILE__ macro
-            cmd_ (Cwd (src </> "plugins" </> "octagram")) Shell "sed -i '13s|^set(plugin_name.*|target_compile_options(rime-octagram-objs PRIVATE \"-ffile-prefix-map=${CMAKE_SOURCE_DIR}=.\")\\n\\0|' CMakeLists.txt"
-            -- remove command line tools
-            cmd_ (Cwd (src </> "plugins" </> "octagram")) Shell "sed -i 19{/add_subdirectory\\(tools\\)/d} CMakeLists.txt"
-            -- remove absolute path by __FILE__ macro
-            cmd_ (Cwd src) Shell "sed -i '143s|target_link_libraries(rime-static.*|target_compile_options(rime-static PRIVATE \"-ffile-prefix-map=${CMAKE_SOURCE_DIR}=.\")\\n\\0|' src/CMakeLists.txt",
+            cmd_ (Cwd src) "git checkout ."
+            cmd_ (Cwd src) "git apply ../patches/librime.patch",
           cmakeFlags = \BuildEnv {..} ->
             [ "-DBUILD_SHARED_LIBS=OFF",
               "-DBUILD_STATIC=ON",
@@ -55,7 +54,8 @@ librimeRule = do
                         "yaml-cpp",
                         "leveldb",
                         "marisa",
-                        "opencc"
+                        "opencc",
+                        "lua"
                       ]
                       <> ["."]
                   )
